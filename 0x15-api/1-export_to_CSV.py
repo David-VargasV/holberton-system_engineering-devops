b2afc5871078a493ded247fe4ed6359dfbcdaabf
David-vargasV <4523@holbertonschool.com>
2022-09-09 14:23:12 -0500

    Task_1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Using the task #0, to export data in the CSV format.
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = sys.argv[1]
+    user = requests.get(url + "users/{}".format(userId)).json()
+    todo = requests.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            taskwriter.writerow([int(userId), user.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
