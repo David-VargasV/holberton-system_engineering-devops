ed56717437b88f72263a3289db5f240f99b7ac74
David-vargasV <4523@holbertonschool.com>
2022-09-09 12:00:29 -0500

    Task_0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script for a given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1]), verify=False).json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    
+    completed = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(todo)))
+    print("\t {}".format(task) for task in completed)
