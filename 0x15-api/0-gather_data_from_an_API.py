82b426d21c6894bc89a4f55b871ad10448a8abce
David-vargasV <4523@holbertonschool.com>
2022-09-09 12:35:47 -0500

    Task_0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,8 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(argv[1]), verify=False).json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(argv[1]), verify=False).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
                         format(argv[1]), verify=False).json()
 
