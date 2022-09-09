b28630b74a6630271b2b6b8d1085243f86f0537c
David-vargasV <4523@holbertonschool.com>
2022-09-09 12:22:40 -0500

    Task_0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(argv[1]), verify=False).json()
     todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
-    
+
     completed = []
     for task in todo:
         if task.get('completed') is True:
