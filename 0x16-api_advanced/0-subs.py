8be875f5d7c6ffe1f30e722384a599fb86cc0a67
David-vargasV <4523@holbertonschool.com>
2022-09-09 21:42:38 -0500

    Executable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,5 +3,23 @@
 How many subscribers?
 """
 
+import imp
+from wsgiref import headers
+import requests
+
+
 def number_of_subscribers(subreddit):
-    
+    """ """
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    user = 'reddit_user'
+    headers = {'User-Agent': user}
+
+    req = requests.get(url, headers=headers, allow_redirects=False)
+    if req.status_code == 404:
+        return 0
+
+    data = req.json()['data']
+    p_list = data['children']
+    p_data = p_list[0]['data']
+
+    return p_data['subreddit_subscribers']
