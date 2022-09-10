b230112dfcbf3c47e1f1332e9ccc3379be85c694
David-vargasV <4523@holbertonschool.com>
2022-09-10 13:48:35 -0500

    Task_1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,4 +3,21 @@
 Top Ten
 """
 
+import requests
+
+
 def top_ten(subreddit):
+    """  """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user = 'reddit_user'
+    headers = {'User-Agent': user}
+
+    req = requests.get(url, headers=headers, allow_redirects=False)
+    if req.status_code == 404:
+        print('None')
+    else:
+        data = req.json()['data']
+        p_list = data['children']
+
+        for p in p_list[0:10]:
+            print(p['data']['title'])
